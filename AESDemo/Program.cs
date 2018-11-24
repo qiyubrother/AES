fd01574ee8d9baed486edcb41ae857aff5649870
qiyubrother <13240137763@163.com>
2018-11-24 09:12:20 +0800

    添加项目文件。

---
+++ AESDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "123456123456";
+            var keyBytes = Encoding.ASCII.GetBytes(key);
+            var originText = "北京，你好。";
+            // 加密原文
+            var encBytes = qiyubrother.AES.Encrypt(Encoding.UTF8.GetBytes(originText), keyBytes);
+            // 解密密文
+            var decBytes = qiyubrother.AES.Decrypt(encBytes, keyBytes);
+            var rstOriginText = Encoding.UTF8.GetString(decBytes);
+
+            Console.WriteLine($"原文：“{originText}”");
+            Console.WriteLine($"还原：“{rstOriginText}”");
+
+            Console.ReadKey();
+        }
+    }
+}
